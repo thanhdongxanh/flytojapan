2483dd113d80bf18bac47cda55c86f3429b84b64
thanhdongxanh <phamvietsanggtvt.hanoi@gmail.com>
2021-01-02 12:10:19 +0700

    up len toan bo

--- 1.js
+++ 1.js
@@ -1,4 +1,26 @@
 ﻿ $(function(){
- 
-})  
- 
+    $(".xuong, .kn").click(function() {
+ 	$("body,html").animate({scrollTop: $('#khoiedu').offset().top},800)
+ 	return false ;
+  }) 
+    $(".ct").click(function() {
+ 	$("body,html").animate({scrollTop: $('#khoiedu').offset().top},800)
+ 	return false ;
+  })  
+   $(".ph").click(function() {
+ 	$("body,html").animate({scrollTop: $('#phanhoi').offset().top},800)
+ 	return false ;
+  }) 
+   $(".up").click(function() {
+ 	$("body,html").animate({scrollTop: $('#toup').offset().top},200)
+ 	return false ;
+  })  
+   $(".tva").click(function() {
+ 	$("body,html").animate({scrollTop: $('#tvanh').offset().top},800)
+ 	return false ;
+  }) 
+   $(".ctr").click(function() {
+ 	$("body,html").animate({scrollTop: $('#vctrinh').offset().top},800)
+ 	return false ;
+  }) 
+ })
